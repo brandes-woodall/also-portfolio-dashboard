50f810c7f2ca489cada78261860395bfb419bc2d
Brandes Woodall <brandes@alsocapital.com>
2026-03-20 17:26:25 -0400

    middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const cookie = req.cookies.get('auth')?.value;
   if (cookie === process.env.DASHBOARD_PASSWORD) return NextResponse.next();
 
